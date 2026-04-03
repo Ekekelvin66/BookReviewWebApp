246c7948619b91cb7d90e41212bb875ea575af94
Ekekelvin66 <ekekelvin66@gmail.com>
2026-04-04 00:50:32 +0200

    Fix: Add DATABASE_URL and SSL support for Render

--- db.js
+++ db.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export const db = new pg.Pool({
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-    host:process.env.DB_HOST,
-    port:Number(process.env.DB_PORT),
+    // user:process.env.DB_USER,
+    // password:process.env.DB_PASSWORD,
+    // database:process.env.DB_NAME,
+    // host:process.env.DB_HOST,
+    // port:Number(process.env.DB_PORT),
+    connectionString: process.env.DATABASE_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 db.connect();
 
